249498e11c34235e883ee6bbd27e32980c9f508e
shubhank25 <shubhanksingh565@gmail.com>
2021-07-25 16:58:12 +0530

    commit

--- backend/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const config = require('../config');
-
-mongoose.connect(config.con_url)
-    .then((data) => { console.log('Database Connected') })
-    .catch((er) => { console.error(er) });
-
-module.exports = mongoose
