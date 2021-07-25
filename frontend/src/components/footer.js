1a708147c09585d7ffe75edd63deb7f2a9b743c4
anupam7007r <anupamrastogi043@gmail.com>
2021-07-25 15:25:44 +0530

    ..

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -4,18 +4,6 @@ const Footer = () => {
 
 
     return (
-        <div class="copyright-area">
-      <div class="container">
-         <div class="row">
-            <div class="col-lg-6 col-md-6">
-               <ul>
-                  <li><a href="privacy">Privacy Policy</a></li>
-                  <li><a href="cookie" >Cookie Policy</a></li>
-               </ul>
-            </div>
-            <div class="col-lg-6 col-md-6 text-right">
-              <p> © <script type="text/JavaScript">document.write(new Date().getFullYear());</script>2021. All rights reserved to DiGiCart Solutions Ltd.</p>
-            </div>
         <div class="main-footer">
       <div class="container">
          <div class="row">
@@ -40,7 +28,6 @@ const Footer = () => {
             
               © 2021-2022 Digicart.com</p>
             </div>
->>>>>>> f1d8584bb3b156f4c75825d0baf84861bb8e47cf
          </div>
       </div>
    </div>
